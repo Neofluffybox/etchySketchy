d96549a9f9e187c52a80d184e770d9e3f05f439d
Nicholas Watson <neonicc95@gmail.com>
2020-11-20 20:30:15 -0700

    fully functional responsive sketch game :)

--- scripts/scripty.js
+++ scripts/scripty.js
@@ -1,8 +1,11 @@
 const resetBtn = document.querySelector('#reset')
-const box16 = document.querySelector('#b16')
 const singleBox = document.querySelector('.box')
-const clear = document.getElementsByClassName('box');
-const container = document.querySelector('.container');
+const clear = document.getElementsByClassName('box')
+const article = document.querySelector('#article')
+const container = document.querySelector('.container')
+const box16 = document.querySelector('#b16')
+const box30 = document.querySelector('#b30')
+const box60 = document.querySelector('#b60')
 
 function colorChanger(color) {
     singleBox.style.backgroundColor = `${color}`;
@@ -13,25 +16,54 @@ function clearArea() {
 }
 
 function createBox(nmbr) {
-    for (let i = 1; i <= nmbr; i++) {
-        let divBox = document.createElement('div');
-        divBox.setAttribute('class', 'box');
-        container.appendChild(divBox);
-        resetBtn.innerHTML = 'Reset';
-    }
+    if (nmbr == 256) {
+        article.style.flexBasis = '289px';
+        article.style.height = '289px';
+        for (let i = 1; i <= nmbr; i++) {
+            let divBox = document.createElement('div');
+            divBox.setAttribute('class', 'box');
+            container.appendChild(divBox);
+            resetBtn.innerHTML = 'Reset';
+        };
+    } else if (nmbr == 900) {
+        article.style.flexBasis = '270px';
+        article.style.height = '270px';
+        for (let i = 1; i <= nmbr; i++) {
+            let divBox = document.createElement('div');
+            divBox.setAttribute('class', 'box');
+            divBox.style.height = '9px';
+            divBox.style.width = '9px';
+            container.appendChild(divBox);
+            resetBtn.innerHTML = 'Reset';
+
+        };
+    } else {
+        article.style.flexBasis = '300px';
+        article.style.height = '300px';
+        for (let i = 1; i <= nmbr; i++) {
+            let divBox = document.createElement('div');
+            divBox.setAttribute('class', 'box');
+            divBox.style.height = '5px';
+            divBox.style.width = '5px';
+            container.appendChild(divBox);
+            resetBtn.innerHTML = 'Reset';
+        };
+
+    };
 }
 
 resetBtn.addEventListener('click', () => {
     clearArea();
-    createBox(256);
 })
 box16.addEventListener('click', () => {
     clearArea();
     createBox(256);
 })
-
-if (singleBox) {
-    singleBox.addEventListener('mouseover', () => {
-        colorChanger(red);
-    });
-}
+box30.addEventListener('click', () => {
+    clearArea();
+    createBox(900);
+})
+box60.addEventListener('click', () => {
+    clearArea();
+    createBox(3600);
+})
